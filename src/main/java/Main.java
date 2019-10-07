0addccf70928ac04780afd6f88b68b4bde3f1e76
Santiago Suarez <santiagosuarez@s4n.co>
2019-10-07 09:17:02 -0500

    Inital commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static List<Integer> generateList(long size) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < size; ++i){
+            list.add(i);
+        }
+        return list;
+    }
+
+    static List<TestClass> genList2(long size){
+        TestClass instance = new TestClass("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        List<TestClass> list = new ArrayList<>();
+        for(long i = 0; i<size; ++i){
+            list.add(instance);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        List<TestClass> generatedList2 = genList2(1000000);
+        long time3 = System.currentTimeMillis();
+        io.vavr.collection.List<TestClass> xyz = io.vavr.collection.List.ofAll(generatedList2);
+        long time4 = System.currentTimeMillis();
+        long totalTime2 = time4 - time3;
+        System.out.println(String.format("First Program took %s milliseconds", totalTime2));
+
+        List<Integer> generatedList = generateList(1000000);
+        long time = System.currentTimeMillis();
+        io.vavr.collection.List<Integer> wxy = io.vavr.collection.List.ofAll(generatedList);
+        long time2 = System.currentTimeMillis();
+        long totalTime = time2 - time;
+        System.out.println(String.format("Second Program took %s milliseconds", totalTime));
+    }
+}
---
+++ src/main/java/TestClass.java
@@ -0,0 +1,19 @@
+public class TestClass {
+    private String attr1;
+    private String attr2;
+    private String attr3;
+    private String attr4;
+    private String attr5;
+    private String attr6;
+    private String attr7;
+
+    public TestClass(String s1, String s2, String s3, String s4, String s5, String s6, String s7){
+        attr1 = s1;
+        attr2 = s2;
+        attr3 = s3;
+        attr4 = s4;
+        attr5 = s5;
+        attr6 = s6;
+        attr7 = s7;
+    }
+}
